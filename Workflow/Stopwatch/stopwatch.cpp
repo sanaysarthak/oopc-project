9e68c9e27490f9e0568a5191606bc0b45e648cb3
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-05-28 16:53:30 +0530

    Update stopwatch.cpp

--- Workflow/Stopwatch/stopwatch.cpp
+++ Workflow/Stopwatch/stopwatch.cpp
@@ -4,38 +4,38 @@
 
 using namespace std;
 
-void displayTime(int hours, int minutes, int seconds) {
+void displayTime(int minutes, int seconds, int milliseconds) {
     system("cls"); // for windows 
     // system("clear"); // for linux
-    
-    cout << hours << ":" << minutes << ":" << seconds << endl;
+    cout << "STOPWATCH in C++\n" << endl;
+    cout << minutes << ":" << seconds << ":" << milliseconds << endl;
 }
 
 int main() {
-    int hour, min, sec = 0; // initializing with 0
-    cout << "Stopwatch in C++.\n" << endl;
+    int min, sec, m_sec = 0; // initializing with 0
+    cout << "Stopwatch in C++\n" << endl;
     cout << "Press ENTER to START Stopwatch.\nPress Ctrl + C to STOP Stopwatch." << endl;
     cin.ignore();
 
-    displayTime(hour, min, sec);
+    displayTime(min, sec, m_sec);
 
     while(true) {
         Sleep(1);
 
-        sec++;
+        m_sec++;
+
+        if(m_sec > 59) {
+            sec++;
+            m_sec = 0;
+        }
 
         if(sec > 59) {
             min++;
+            m_sec = 0;
             sec = 0;
         }
 
-        if(min > 59) {
-            hour++;
-            sec = 0;
-            min = 0;
-        }
-
-        displayTime(hour, min, sec);
+        displayTime(min, sec, m_sec);
     }
     return 0;
 }
