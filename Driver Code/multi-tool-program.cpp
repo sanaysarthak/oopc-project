006e4417f228fe8dab3573da18da18f32946f030
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2024-05-31 10:51:34 +0530

    Update multi-tool-program.cpp

--- Driver Code/multi-tool-program.cpp
+++ Driver Code/multi-tool-program.cpp
@@ -11,71 +11,121 @@ using namespace std;
 const char alphanum[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int pool_len = sizeof(alphanum) - 1; // word pool length
 
+class Display {
+    public:
+        void displayTime(int minutes, int seconds, int milliseconds)
+        {
+            system("cls"); // for windows
+            // system("clear"); // for linux
+
+            cout << "STOPWATCH in C++\n" << endl;
+            cout << minutes << ":" << seconds << ":" << milliseconds << endl;
+        }
+
+        void displayTime(int hours, int minutes, int seconds, double x)
+        {
+            system("cls"); // for windows
+            // system("clear"); // for linux
+
+            cout << "TIMER in C++\n" << endl;
+            cout << hours << ":" << minutes << ":" << seconds << endl;
+        }
+};
+
+class ASCII : public Display {
+    public:
+        void showASCII() {
+            cout << "                                                                                                        " << endl;
+            cout << "             ________  ________  ________  ________                                                     " << endl;
+            cout << "            |\\   __  \\|\\   __  \\|\\   __  \\|\\   ____\\                                            " << endl;
+            cout << "            \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\___|                                     " << endl;
+            cout << "             \\ \\  \\\\\\  \\ \\  \\\\\\  \\ \\   ____\\ \\  \\                                        " << endl;
+            cout << "              \\ \\  \\\\\\  \\ \\  \\\\\\  \\ \\  \\___|\\ \\  \\____                                  " << endl;
+            cout << "               \\ \\_______\\ \\_______\\ \\__\\    \\ \\_______\\                                      " << endl;
+            cout << "                \\|_______|\\|_______|\\|__|     \\|_______|                                            " << endl;
+            cout << "                                                                                                        " << endl;
+            cout << "                                                                                                        " << endl;
+            cout << "  ________  ________  ________        ___  _______   ________ _________                                 " << endl;
+            cout << " |\\   __  \\|\\   __  \\|\\   __  \\      |\\  \\|\\  ___ \\ |\\   ____\\\\___   ___\\                 " << endl;
+            cout << " \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\|\\  \\     \\ \\  \\ \\   __/|\\ \\  \\___\\|___ \\  \\_|        " << endl;
+            cout << "  \\ \\   ____\\ \\   _  _\\ \\  \\\\\\  \\  __ \\ \\  \\ \\  \\_|/_\\ \\  \\       \\ \\  \\           " << endl;
+            cout << "   \\ \\  \\___|\\ \\  \\\\  \\\\ \\  \\\\\\  \\|\\  \\\\_\\  \\ \\  \\_|\\ \\ \\  \\____   \\ \\  \\   " << endl;
+            cout << "    \\ \\__\\    \\ \\__\\\\ _\\\\ \\_______\\ \\________\\ \\_______\\ \\_______\\  \\ \\__\\          " << endl;
+            cout << "     \\|__|     \\|__|\\|__|\\|_______|\\|________|\\|_______|\\|_______|   \\|__|                      " << endl;
+            cout << "                                                                                                        " << endl;
+
+            cout << "\n\nCreated By :- " << endl;
+            cout << "Sarthak Sanay \t\t 230031101611051" << endl;
+            cout << "Ashwani Gupta \t\t 230031101611049" << endl;
+            cout << "Mudit Srivastav \t 230031101611053\n\n" << endl;
+        }
+
+        void showToolInfo() {
+            cout << "\n======================================" << endl;
+            cout << "||        Multi-Tool Program        ||" << endl;
+            cout << "======================================" << endl;
+            cout << "||  1. File Encryptor & Decryptor   ||" << endl;
+            cout << "||  2. Password Generator           ||" << endl;
+            cout << "||  3. Stopwatch                    ||" << endl;
+            cout << "||  4. Countdown Timer              ||" << endl;
+            cout << "||  0. Quit                         ||" << endl;
+            cout << "======================================" << endl;
+            cout << "\nEnter your choice: ";
+        }
+};
+
+class Encryption {
+    public:
+        void encryptFile(const string &inputFilePath, const string &outputFilePath, int key)
+        {
+            ifstream inputFile(inputFilePath, ios::binary);
+            ofstream outputFile(outputFilePath, ios::binary);
+
+            char ch;
+            while (inputFile.get(ch))
+            {
+                outputFile.put(ch + key);
+            }
+
+            inputFile.close();
+            outputFile.close();
+            cout << "\nFile encrypted successfully." << endl;
+        }
+
+        void decryptFile(const string &inputFilePath, const string &outputFilePath, int key)
+        {
+            ifstream inputFile(inputFilePath, ios::binary);
+            ofstream outputFile(outputFilePath, ios::binary);
+
+            char ch;
+            while (inputFile.get(ch))
+            {
+                outputFile.put(ch - key);
+            }
+
+            inputFile.close();
+            outputFile.close();
+            cout << "\nFile decrypted successfully." << endl;
+        }
+};
+
 void showASCII();
 void showToolInfo();
 
-void encryptFile(const string &inputFilePath, const string &outputFilePath, int key)
-{
-    ifstream inputFile(inputFilePath, ios::binary);
-    ofstream outputFile(outputFilePath, ios::binary);
-
-    char ch;
-    while (inputFile.get(ch))
-    {
-        outputFile.put(ch + key);
-    }
-
-    inputFile.close();
-    outputFile.close();
-    cout << "\nFile encrypted successfully." << endl;
-}
-
-void decryptFile(const string &inputFilePath, const string &outputFilePath, int key)
-{
-    ifstream inputFile(inputFilePath, ios::binary);
-    ofstream outputFile(outputFilePath, ios::binary);
-
-    char ch;
-    while (inputFile.get(ch))
-    {
-        outputFile.put(ch - key);
-    }
-
-    inputFile.close();
-    outputFile.close();
-    cout << "\nFile decrypted successfully." << endl;
-}
-
-void displayTime(int minutes, int seconds, int milliseconds)
-{
-    system("cls"); // for windows
-    // system("clear"); // for linux
-
-    cout << "STOPWATCH in C++\n" << endl;
-    cout << minutes << ":" << seconds << ":" << milliseconds << endl;
-}
-
-void Timer(int hours, int minutes, int seconds)
-{
-    system("cls"); // for windows
-    // system("clear"); // for linux
-
-    cout << "TIMER in C++\n" << endl;
-    cout << hours << ":" << minutes << ":" << seconds << endl;
-}
-
 int main()
 {
+    ASCII a1;
+    Encryption e1;
     int ch;
     string inputFilePath, outputFilePath; // for file encryptor and decryptor
     int pass_len; // for password generator
     int choice, key; // for file encryptor and decryptor
     int hour, min, sec, m_sec = 0; // initializing with 0
 
-    showASCII();
+    a1.showASCII();
 
     do {
-        showToolInfo();
+        a1.showToolInfo();
         cin >> ch;
         switch (ch) {
 
@@ -96,9 +146,9 @@ int main()
                 cin >> outputFilePath;
 
                 if (choice == 1)
-                    encryptFile(inputFilePath, outputFilePath, key);
+                    e1.encryptFile(inputFilePath, outputFilePath, key);
                 else if (choice == 2)
-                    decryptFile(inputFilePath, outputFilePath, key);
+                    e1.decryptFile(inputFilePath, outputFilePath, key);
                 else
                     cout << "Invalid choice." << endl;
                 cout << "\n" << endl;
@@ -134,7 +184,7 @@ int main()
                 cout << "Press ENTER to START Stopwatch.\nPress Ctrl + C to STOP Stopwatch." << endl;
                 cin.ignore();
                 cin.ignore(); // to take Enter as input
-                displayTime(min, sec, m_sec);
+                a1.displayTime(min, sec, m_sec);
                 while (true)
                 {
                     Sleep(1);
@@ -150,7 +200,7 @@ int main()
                         m_sec = 0;
                         sec = 0;
                     }
-                    displayTime(min, sec, m_sec);
+                    a1.displayTime(min, sec, m_sec);
                 }
                 cout << "\n" << endl; 
                 break;
@@ -165,7 +215,7 @@ int main()
                 cin >> min;
                 cout << "Enter second: ";
                 cin >> sec;
-                Timer(hour, min, sec);
+                a1.displayTime(hour, min, sec, 0.0);
                 while (true)
                 {
                     Sleep(1000);
@@ -180,55 +230,24 @@ int main()
                         }
                         else
                             min--;
-                        sec = 59;
+                            sec = 59;
                     }
                     else
                         sec--;
-                    Timer(hour, min, sec);
+                    a1.displayTime(hour, min, sec, 0.0);
                 }
                 cout << "\n" << endl; 
                 break;
+
+            case 0:
+                cout << "\nExited the program succesfully!\n" << endl;
+                break;
+
+            default:
+                cout << "Enter correct choice. (1 to 4).\n" << endl;
         }
+
     } while(ch != 0);
 
     return 0;
-}
-
-void showASCII() {
-    cout << "                                                                                                        " << endl;
-    cout << "             ________  ________  ________  ________                                                     " << endl;
-    cout << "            |\\   __  \\|\\   __  \\|\\   __  \\|\\   ____\\                                            " << endl;
-    cout << "            \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\___|                                     " << endl;
-    cout << "             \\ \\  \\\\\\  \\ \\  \\\\\\  \\ \\   ____\\ \\  \\                                        " << endl;
-    cout << "              \\ \\  \\\\\\  \\ \\  \\\\\\  \\ \\  \\___|\\ \\  \\____                                  " << endl;
-    cout << "               \\ \\_______\\ \\_______\\ \\__\\    \\ \\_______\\                                      " << endl;
-    cout << "                \\|_______|\\|_______|\\|__|     \\|_______|                                            " << endl;
-    cout << "                                                                                                        " << endl;
-    cout << "                                                                                                        " << endl;
-    cout << "  ________  ________  ________        ___  _______   ________ _________                                 " << endl;
-    cout << " |\\   __  \\|\\   __  \\|\\   __  \\      |\\  \\|\\  ___ \\ |\\   ____\\\\___   ___\\                 " << endl;
-    cout << " \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\|\\  \\     \\ \\  \\ \\   __/|\\ \\  \\___\\|___ \\  \\_|        " << endl;
-    cout << "  \\ \\   ____\\ \\   _  _\\ \\  \\\\\\  \\  __ \\ \\  \\ \\  \\_|/_\\ \\  \\       \\ \\  \\           " << endl;
-    cout << "   \\ \\  \\___|\\ \\  \\\\  \\\\ \\  \\\\\\  \\|\\  \\\\_\\  \\ \\  \\_|\\ \\ \\  \\____   \\ \\  \\   " << endl;
-    cout << "    \\ \\__\\    \\ \\__\\\\ _\\\\ \\_______\\ \\________\\ \\_______\\ \\_______\\  \\ \\__\\          " << endl;
-    cout << "     \\|__|     \\|__|\\|__|\\|_______|\\|________|\\|_______|\\|_______|   \\|__|                      " << endl;
-    cout << "                                                                                                        " << endl;
-
-    cout << "\n\nCreated By :- " << endl;
-    cout << "Sarthak Sanay \t\t 230031101611051" << endl;
-    cout << "Ashwani Gupta \t\t 230031101611049" << endl;
-    cout << "Mudit Srivastav \t 230031101611053\n\n" << endl;
-}
-
-void showToolInfo() {
-    cout << "\n======================================" << endl;
-    cout << "||        Multi-Tool Program        ||" << endl;
-    cout << "======================================" << endl;
-    cout << "||  1. File Encryptor & Decryptor   ||" << endl;
-    cout << "||  2. Password Generator           ||" << endl;
-    cout << "||  3. Stopwatch                    ||" << endl;
-    cout << "||  4. Countdown Timer              ||" << endl;
-    cout << "||  0. Quit                         ||" << endl;
-    cout << "======================================" << endl;
-    cout << "\nEnter your choice: ";
 }
